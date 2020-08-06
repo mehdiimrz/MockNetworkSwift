a8cb9328f73e68ff5a698c8ab7fa20ee66763d18
Mehdi Mirzaie <mehdi.mrz@live.com>
2020-08-06 16:27:14 +0430

    NetworkingService added

---
+++ MockNetworkSwift/Networking/NetworkingService.swift
@@ -0,0 +1,32 @@
+//
+//  NetworkingService.swift
+//  MockNetwork
+//
+//  Created by Mehdi on 8/6/20.
+//  Copyright © 2020 Mehdiimrz. All rights reserved.
+//
+
+import Foundation
+
+/// An interface for implementing network functions
+protocol NetworkingService {
+    
+    /**
+    Get home object
+
+    - Parameters:
+       - completion: Return HomeItem in Result
+    */
+    func getHomeFeed(completion: @escaping (Result<HomeItem, Error>)->Void)
+    
+    
+    /**
+    Get comments of post by postID
+
+    - Parameters:
+       - postId: Identifier of a post
+       - completion: Return list of Comments in Result
+    */
+    func getComments(withPostId postId : Int, completion: @escaping (Result<[Comment], Error>)->Void )
+
+}
