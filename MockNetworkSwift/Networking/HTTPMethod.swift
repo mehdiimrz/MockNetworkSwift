7826cbdaab4d4c2db0259784fd0ee3abf5cf3227
Mehdi Mirzaie <mehdi.mrz@live.com>
2020-08-06 17:00:41 +0430

    HTTPMethod added

---
+++ MockNetworkSwift/Networking/HTTPMethod.swift
@@ -0,0 +1,19 @@
+//
+//  HTTPMethod.swift
+//  GiftCard
+//
+//  Created by Mehdi on 8/6/20.
+//  Copyright © 2020 Mehdiimrz. All rights reserved.
+//
+
+import Foundation
+
+/// HTTP methods
+enum HTTPMethod : String{
+    
+    case GET = "GET"
+    case PUT = "PUT"
+    case POST = "POST"
+    case DELETE = "DELETE"
+    
+}
