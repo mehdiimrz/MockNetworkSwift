138b368affe42a9b31d2bbdba878c931e26012ad
Mehdi Mirzaie <mehdi.mrz@live.com>
2020-08-06 16:16:53 +0430

    initial commit

---
+++ MockNetworkSwift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MockNetworkSwift
+//
+//  Created by Mehdi on 8/6/20.
+//  Copyright © 2020 Mehdiimrz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
